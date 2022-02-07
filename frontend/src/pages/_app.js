684de2c82ad7d17995150253191e1bc9e4dc73a2
matthewfreire <matt@justdjango.com>
2022-02-07 15:58:09 +0000

    Nextjs version 12

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -10,16 +10,16 @@ const App = ({ Component, pageProps }) => {
             <Head>
                 <title>HTTPOnly Auth</title>
                 <meta name='viewport' content='width=device-width, inital-scale=1' />
-                <link 
-                    href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' 
-                    rel='stylesheet' 
-                    integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' 
-                    crossorigin='anonymous'
+                <link
+                    href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+                    rel='stylesheet'
+                    integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC'
+                    crossOrigin='anonymous'
                 ></link>
-                <script 
-                    src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' 
-                    integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM' 
-                    crossorigin='anonymous'
+                <script
+                    src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js'
+                    integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM'
+                    crossOrigin='anonymous'
                 ></script>
             </Head>
             <Component {...pageProps} />
